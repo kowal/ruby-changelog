7d598b88ff8b610852cf6917b7d3f1120902e3c6
Marek Kowalcze <marek.kowalcze@gmail.com>
2018-11-03 22:37:00 +0100

    add script for generating static site

---
+++ changelog.rb
@@ -0,0 +1,28 @@
+require 'json'
+require 'erb'
+
+CHANGELOG_SOURCE = 'changelog.json'
+TEMPLATE_HTML = 'templates/index.html.erb'
+OUTPUT_HTML = 'out/index.html'
+
+class Changelog
+  def generate_html
+    ruby_versions = JSON.parse(changelog_content)
+    html_content = ERB.new(template_content, nil, '-')
+      .result_with_hash(ruby_versions: ruby_versions['ruby_versions'])
+    File.write(OUTPUT_HTML, html_content)
+  end
+
+  private
+
+  def changelog_content
+    File.read(CHANGELOG_SOURCE)
+  end
+
+  def template_content
+    File.read(TEMPLATE_HTML)
+  end
+
+end
+
+Changelog.new.generate_html
